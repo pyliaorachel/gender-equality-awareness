7a98a0dedea63df98c2aa36787af333a9ca61090
Peiyu Liao <pyliao@stanford.edu>
2019-04-20 19:37:58 -0700

    Add logo image; update host

--- project/src/app.py
+++ project/src/app.py
@@ -72,4 +72,4 @@ def sent_score(word):
     return str(emb_model.sent_score(word))
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
